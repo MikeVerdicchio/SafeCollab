493fe8f107148c9dcef5148d4d16a7d0669bb40b
Tahiya Salam <ts7ft@virginia.edu>
2016-04-04 14:41:41 -0400

    fixed merge conflicts
    # Conflicts:
    #	SafeCollab/urls.py

--- SafeCollab/urls.py
+++ SafeCollab/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-<<<<<<< HEAD
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
@@ -8,28 +7,16 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'SafeCollab.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # url(r'^$', 'SafeCollab.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'auth.views.login_user'),
+    url(r'^logout/$', 'auth.views.logout_user'),
     url(r'^', include('encrypt.urls', namespace="encrypt")),
     url(r'^encrypt/$', 'encrypt.views.index'),
     url(r'^FileUpload/',include('FileUpload.urls')),
     url(r'^$', RedirectView.as_view(url='/FileUpload/list/', permanent=True)),
     #url(r'^upload/$', 'FileUpload.views.index'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
 
 # admin.autodiscover()
-
-urlpatterns = patterns('',
-                       # Examples:
-                       url(r'^$', 'SafeCollab.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^login/$', 'auth.views.login_user'),
-                       url(r'^logout/$', 'auth.views.logout_user'),
-                       url(r'^', include('encrypt.urls', namespace="encrypt")),
-                       url(r'^encrypt/$', 'encrypt.views.index'),
-                       )
->>>>>>> tahiya.txt/master
