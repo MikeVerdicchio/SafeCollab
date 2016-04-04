edfcfaded2c8fe2771846947fbc3ed014d2eef34
Mike Verdicchio <mikeverdicchio@gmail.com>
2016-04-04 08:22:34 -0400

    Users can now log in and log out with links on the navbar

--- SafeCollab/urls.py
+++ SafeCollab/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'SafeCollab.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'auth.views.login_user'),
-    url(r'^', include('encrypt.urls', namespace="encrypt")),
-    url(r'^encrypt/$', 'encrypt.views.index'),
-)
+                       # Examples:
+                       url(r'^$', 'SafeCollab.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^login/$', 'auth.views.login_user'),
+                       url(r'^logout/$', 'auth.views.logout_user'),
+                       url(r'^', include('encrypt.urls', namespace="encrypt")),
+                       url(r'^encrypt/$', 'encrypt.views.index'),
+                       )
--- auth/views.py
+++ auth/views.py
@@ -1,32 +1,34 @@
 from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import LoginForm
 
 
 def login_user(request):
+    logout(request)
     state = "Login to SafeCollab."
     username = password = ''
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    state = "You're successfully logged in!"
-                else:
-                    state = "Your account is not active, please contact the site admin."
+        # form = LoginForm(request.POST)
+        # if form.is_valid():
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
             else:
-                state = "Your username and/or password were incorrect."
-    else:
-        form = LoginForm()
-    return render(request, 'auth.html', {'form': form, 'state': state, 'username': username, 'password': password})
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+    # else:
+        # form = LoginForm()
+    return render(request, 'auth.html', {'state': state, 'username': username, 'password': password})
 
 
-def logout(request):
+def logout_user(request):
     logout(request)
+    return render(request, 'auth.html')
