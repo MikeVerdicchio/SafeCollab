68e0f36a022cc825f27d61e236b8704f473a71db
Mike Verdicchio <mikeverdicchio@gmail.com>
2016-04-04 01:26:23 -0400

    Added navbar to the top of the page.

--- SafeCollab/urls.py
+++ SafeCollab/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'SafeCollab.views.home', name='home'),
+    url(r'^$', 'SafeCollab.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'auth.views.login_user'),
--- auth/views.py
+++ auth/views.py
@@ -1,25 +1,32 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login
 
+from .forms import LoginForm
+
+
 def login_user(request):
-    logout(request)
     state = "Login to SafeCollab."
     username = password = ''
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                state = "You're successfully logged in!"
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    state = "You're successfully logged in!"
+                else:
+                    state = "Your account is not active, please contact the site admin."
             else:
-                state = "Your account is not active, please contact the site admin."
-        else:
-            state = "Your username and/or password were incorrect."
-    return render_to_response('auth.html', {'state':state, 'username':username}, context_instance=RequestContext(request))
+                state = "Your username and/or password were incorrect."
+    else:
+        form = LoginForm()
+    return render(request, 'auth.html', {'form': form, 'state': state, 'username': username, 'password': password})
+
 
 def logout(request):
     logout(request)
