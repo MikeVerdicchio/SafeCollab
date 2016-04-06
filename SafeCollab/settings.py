7bbb7d72d9ccdc741c2385f8ea87bf8894acade8
Mike Verdicchio <mikeverdicchio@gmail.com>
2016-04-06 16:38:19 -0400

    Users can now log in and log out. Homepage renders when you go to the index.

--- SafeCollab/settings.py
+++ SafeCollab/settings.py
@@ -91,7 +91,6 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, 'templates'),
     os.path.join(PROJECT_ROOT, 'auth/templates'),
--- auth/views.py
+++ auth/views.py
@@ -11,8 +11,6 @@ def login_user(request):
     state = "Login to SafeCollab."
     username = password = ''
     if request.method == 'POST':
-        # form = LoginForm(request.POST)
-        # if form.is_valid():
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
@@ -20,15 +18,21 @@ def login_user(request):
             if user.is_active:
                 login(request, user)
                 state = "You're successfully logged in!"
+                return render(request, 'index.html')
             else:
                 state = "Your account is not active, please contact the site admin."
         else:
             state = "Your username and/or password were incorrect."
-    # else:
-        # form = LoginForm()
     return render(request, 'auth.html', {'state': state, 'username': username, 'password': password})
 
 
 def logout_user(request):
     logout(request)
-    return render(request, 'auth.html')
+    return render(request, 'index.html')
+
+
+def index(request):
+    if request.user.is_authenticated():
+        return render(request, 'index.html')
+    else:
+        return render(request, 'auth.html')
