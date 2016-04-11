9690a0e05c0230a2b6db00e46190dbb1783d4721
Mike Verdicchio <mikeverdicchio@gmail.com>
2016-04-11 00:36:38 -0400

    Registration form complete. Need to add data into database

--- auth/forms.py
+++ auth/forms.py
@@ -3,4 +3,12 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username', max_length=30)
-    password = forms.CharField(label="Password", max_length=30)
+    password = forms.CharField(label='Password')
+
+
+class RegisterForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=30)
+    username = forms.CharField(label='Username', max_length=30)
+    email = forms.EmailField(label='Email', max_length=50)
+    password = forms.CharField(label='Password')
+    confirm = forms.CharField(label='Confirm Password')
