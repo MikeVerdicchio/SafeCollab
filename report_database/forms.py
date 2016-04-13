68fdae5af518d2a41d5976d39ed146fbd7a4c6f8
mliao95 <ml4zz@virginia.edu>
2016-04-13 03:46:24 -0400

    Report Database Ready!

---
+++ report_database/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms.extras.widgets import SelectDateWidget
+
+class ReportForm(forms.Form):
+    report_name = forms.CharField(label='Report Name', max_length=50)
+    date = forms.DateField(widget=SelectDateWidget(years=[ str(k) for k in range(2000,2051)]))
+    sdesc = forms.CharField(label='Short Description', max_length=60)
+    ldesc = forms.CharField(label='Long Description', widget=forms.Textarea, max_length=1000)
+    private = forms.BooleanField(label='Private', required=False)
+    file_1 = forms.FileField(label='File_1', required=False)
+    file_2 = forms.FileField(label='File_2', required=False)
+    file_3 = forms.FileField(label='File_3', required=False)
