a29d67fdee6ed2004a8098533e1e6893179284e4
mliao95 <ml4zz@virginia.edu>
2016-04-18 14:11:12 -0400

    fixing merge conflicts for report database

--- auth/views.py
+++ auth/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from auth.models import UserProfile
 from .forms import RegisterForm, LoginForm
-
+from Crypto.PublicKey import RSA
+from Crypto import Random
 
 def login_user(request):
     if request.method == 'POST':
@@ -28,6 +30,10 @@ def logout_user(request):
     logout(request)
     return render(request, 'index.html')
 
+def key_generation():
+    random_generator = Random.new().read
+    key = RSA.generate(1024, random_generator)
+    return key
 
 def register_user(request):
     if request.method == 'POST':
@@ -39,11 +45,17 @@ def register_user(request):
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             confirm = form.cleaned_data['confirm']
+            site_manager = False
             if password == confirm:
                 user = User.objects.create_user(username=username, email=email, password=password, first_name=first, last_name=last)
                 user.save()
+                key = key_generation()
+                public_key = key.publickey().exportKey('PEM')
+                user_profile = UserProfile.objects.create(username=username, site_manager=site_manager, public_key=public_key)
+                user_profile.save()
                 return render(request, 'index.html')
     else:
         form = RegisterForm()
 
     return render(request, 'register.html', {'form': form})
+
