6bfe12aea1a16082fb8118af35581547f36d95d7
Tahiya Salam <ts7ft@virginia.edu>
2016-04-30 18:10:35 -0400

    final changes to encryption

--- auth/views.py
+++ auth/views.py
@@ -69,19 +69,18 @@ def register_user(request):
                 user.save()
                 key = key_generation()
                 public_key = key.publickey().exportKey()
-                print(public_key)
                 private_key = key.exportKey()
-                print(private_key)
                 filename = os.getcwd()+'/auth/key.pem'
-                f = open(filename,'wb')
-                f.write(private_key)
+                f = open(filename,'w')
+                f.write(private_key.decode('utf-8'))
+                f.close()
                 user = User.objects.get(username=username)
                 user_profile = UserProfile.objects.create(user=user, username=username, site_manager=site_manager,
                                                           public_key=public_key)
                 user_profile.save()
                 message = 'Hi ' + first + ',\n\nThank you for signing up for SafeCollab!\n\nBest,\nThe SafeCollab Team'
                 email = EmailMessage('Welcome to SafeCollab', message, to=[email])
-                email.attach_file(filename, "application/text")
+                email.attach_file(filename)
                 email.send()
                 f = open(filename,'w+')
                 f.close()
